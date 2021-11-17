a11507eff4568065eb91e8ee0273bb2b763f3280
philipjhasondeleon <jhasondeleon@gmail.com>
2021-11-16 21:42:55 -0500

    Connected to FireBase

--- app/src/main/java/com/example/otwfoods/AdministratorUser/AdministratorSignInActivity.java
+++ app/src/main/java/com/example/otwfoods/AdministratorUser/AdministratorSignInActivity.java
@@ -1,16 +1,89 @@
 package com.example.otwfoods.AdministratorUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.otwfoods.AdministratorUser.Model.UserAdministrator;
 import com.example.otwfoods.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdministratorSignInActivity extends AppCompatActivity {
+
+    TextInputEditText edtPhone, edtPassword;
+    MaterialButton btnSignInAppAdmin;
+    FirebaseDatabase database;
+    DatabaseReference appAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator_sign_in);
+
+        edtPhone = findViewById(R.id.tiet_administrator_user_phone);
+        edtPassword = findViewById(R.id.tiet_administrator_user_password);
+
+        database = FirebaseDatabase.getInstance();
+        appAdmin = database.getReference("Administrator");
+
+        btnSignInAppAdmin = findViewById(R.id.btn_administrator_user_Sign_in);
+        btnSignInAppAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btnSignInAppAdministrator(edtPhone.getText().toString(),edtPassword.getText().toString());
+            }
+        });
+    }
+
+    private void btnSignInAppAdministrator(String phone, String password){
+
+        ProgressDialog mDialog = new ProgressDialog(AdministratorSignInActivity.this);
+        mDialog.setMessage("Please waiting..");
+        mDialog.show();
+
+        String localPhone = phone;
+        String localPassword = password;
+
+        appAdmin.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(localPhone).exists()) {
+
+                    mDialog.dismiss();
+                    UserAdministrator user = snapshot.child(localPhone).getValue(UserAdministrator.class);
+                    user.setPhone(localPhone);
+                    if(user.getPassword().equals(localPassword)){
+                        Intent login = new Intent(AdministratorSignInActivity.this, AddRestaurantActivity.class);
+                        ConstantAdmin.currentUser = user;
+                        startActivity(login);
+                        finish();
+
+                    }else {
+                        Toast.makeText(AdministratorSignInActivity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(AdministratorSignInActivity.this, "User not Exist in Database", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/otwfoods/AdministratorUser/ConstantAdmin.java
@@ -0,0 +1,7 @@
+package com.example.otwfoods.AdministratorUser;
+
+import com.example.otwfoods.AdministratorUser.Model.UserAdministrator;
+
+public class ConstantAdmin {
+    public static UserAdministrator currentUser;
+}
